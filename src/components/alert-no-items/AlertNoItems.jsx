8dc03d11ff29ed5d948f73d71f503fb18b4b1236
vladimirDav <Vladimir.davii@gmail.com>
2017-09-27 23:11:41 +0300

    fix styles and change alert title no found items

--- src/components/alert-no-items/AlertNoItems.jsx
+++ src/components/alert-no-items/AlertNoItems.jsx
@@ -3,7 +3,11 @@ import './alert-no-items.css';
 
 class AlertNoItems extends React.Component {
 	render() {
-		if (this.props.myArray.length === 0 || this.props.filterArrItemsInputSearch.length === 0) return <h3 className="alert">No <span className="alert__sub">items!</span> Please add some <span className="alert__sub">items!</span></h3>;
+		if (this.props.myArray.length === 0) {
+			return <h3 className="alert">No <span className="alert__sub">items!</span> Please add some <span className="alert__sub">items!</span></h3>
+		} else if (this.props.filterArrItemsInputSearch.length === 0) {
+			return <h3 className="alert"><span className="alert__sub">No</span> items found <span className="alert__sub">!!!</span></h3>
+		}
 		return null;
 	}
 }
